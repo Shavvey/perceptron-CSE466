98a7c027b2021f8fb125258352ba1e3ca846ef59
Cole Johnson <coledjohn@gmail.com>
2025-10-23 14:40:23 -0600

    separate training methods in `main.py`

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from neuron import Neuron
 from stats import ConfusionMatrix, Stats
 from boolean import Boolean
 
-DATA_CLASS = "setosa"
+DATA_CLASS = "versicolor"
+
 
 def print_statistics(preds: list[Boolean], actuals: list[Boolean]):
     cm = ConfusionMatrix(preds, actuals)
@@ -11,15 +12,17 @@ def print_statistics(preds: list[Boolean], actuals: list[Boolean]):
     print(f"Incorrect %: {Stats.percent_incorrect(preds, actuals):.2f}")
     print(f"F1 Score: {Stats.f1_score(preds, actuals):.2f}")
     print(f"Precision: {cm.precision()}\nRecall: {cm.recall():.2f}")
-    
 
-def main():
-    FEATURES = [i for i in range (0, 4)]
-    best_neuron: Neuron = Neuron(0) # find best neuron from picked features
-    best_features = [] # find best features 
-    best_f1_score = 0 # use f1 score to find optimal features + neuron config
-    for first in FEATURES: # pick first feature
-        for second in [feature for feature in FEATURES if feature != first]: # pick second feature
+
+def train_two_features():
+    FEATURES = [i for i in range(0, 4)]
+    best_neuron: Neuron = Neuron(0)  # find best neuron from picked features
+    best_features = []  # find best features
+    best_f1_score = 0  # use f1 score to find optimal features + neuron config
+    for first in FEATURES:  # pick first feature
+        for second in [
+            feature for feature in FEATURES if feature != first
+        ]:  # pick second feature
             feature_pair = [first, second]
             train_data = get_data(DATA_CLASS, 39, feature_pair)
             test_data = get_data(DATA_CLASS, features=feature_pair)
@@ -39,7 +42,23 @@ def main():
     print_statistics(preds, actuals)
     print(f"Best features: {best_features}")
     print(best_neuron)
-    
+
+
+def train_all_features():
+    train_data = get_data(DATA_CLASS, 39)
+    test_data = get_data(DATA_CLASS)
+    n = Neuron(len(train_data[0]) - 1)
+    n.train(train_data, 100)
+    preds = n.test(test_data)
+    actuals = get_actuals(test_data)
+    print_statistics(preds, actuals)
+
+
+def main():
+    train_two_features()
+    print("ALL FEATURES TRAINING")
+    train_all_features()
+
 
 if __name__ == "__main__":
     main()
