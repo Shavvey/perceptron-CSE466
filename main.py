b9defd2d52d1b8cd63a178f07f9e6ff2bc22f20d
Cole Johnson <coledjohn@gmail.com>
2025-10-23 00:37:57 -0600

    Making code to answer question 2

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
 from data import get_actuals, get_data
 from neuron import Neuron
 from stats import ConfusionMatrix, Stats
+from boolean import Boolean
 
-DATA_CLASS = "versicolor"
+DATA_CLASS = "setosa"
 
-def main():
-    train_data = get_data(DATA_CLASS, 39)
-    # create neuron, makes weights the same as the number of features
-    n = Neuron(len(train_data[0]) - 1)
-    n.train(train_data, 1000)
-    test_data = get_data(DATA_CLASS)
-    preds = n.test(test_data)
-    actuals = get_actuals(test_data)
+def print_statistics(preds: list[Boolean], actuals: list[Boolean]):
     cm = ConfusionMatrix(preds, actuals)
     print(cm)
-    pi = Stats.percent_incorrect(preds, actuals)
-    print(f"% Incorrect: {pi:.2f}")
-    f1 = Stats.f1_score(preds, actuals)
-    print(f"F1 Score: {f1}")
-    print("Precision: ", cm.precision())
-    print("Recall: ", cm.recall())
-    print(n)
+    print(f"Incorrect %: {Stats.percent_incorrect(preds, actuals):.2f}")
+    print(f"F1 Score: {Stats.f1_score(preds, actuals):.2f}")
+    print(f"Precision: {cm.precision()}\nRecall: {cm.recall():.2f}")
+    
+
+def main():
+    FEATURES = [i for i in range (0, 4)]
+    best_neuron: Neuron = Neuron(0) # find best neuron from picked features
+    best_features = [] # find best features 
+    best_f1_score = 0 # use f1 score to find optimal features + neuron config
+    for first in FEATURES: # pick first feature
+        for second in [feature for feature in FEATURES if feature != first]: # pick second feature
+            feature_pair = [first, second]
+            train_data = get_data(DATA_CLASS, 39, feature_pair)
+            test_data = get_data(DATA_CLASS, features=feature_pair)
+            n = Neuron(len(train_data[0]) - 1)
+            n.train(train_data, 100)
+            preds = n.test(test_data)
+            actuals = get_actuals(test_data)
+            f1_score = Stats.f1_score(preds, actuals)
+            if f1_score > best_f1_score:
+                best_f1_score = f1_score
+                best_neuron = n
+                best_features = feature_pair
+    # get predictions for the best neuron
+    test_data = get_data(DATA_CLASS, features=best_features)
+    preds = best_neuron.test(test_data)
+    actuals = get_actuals(test_data)
+    print_statistics(preds, actuals)
+    print(f"Best features: {best_features}")
+    print(best_neuron)
+    
 
 if __name__ == "__main__":
     main()
